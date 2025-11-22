48e4a279c94bca85e80b74efca530f391020db95
alex <alex.schmidt767@gmail.com>
2025-11-22 05:31:12 -0600

    added early version of navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import CurrentUserProvider from './context/CurrentUser';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <CurrentUserProvider>
-        
+        <Router>
+          <Navigation/>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+          </Routes>
+        </Router>
       </CurrentUserProvider>
     </div>
   );
---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+const Navigation = () => {
+    const currentUser = useContext(currentUser);
+
+    const navBar = (
+        <Navbar expand="lg">
+            <Container>
+                {
+                    // check for a currently logged in user to decide which navbar to show
+                    currentUser
+                    ? (
+                        <Nav>
+                            <Nav.Link href="/my-pods">Pods</Nav.Link>
+                            <Nav.Link href="/log-new-game">Log a Game</Nav.Link>
+                            <Nav.Link href="/my-profile">{currentUser.username}</Nav.Link>
+                        </Nav>
+                    ) : (
+                        <Nav>
+                            <Nav.Link href="/login">Log In</Nav.Link>
+                            <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        </Nav>
+                    )
+                }
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation;
