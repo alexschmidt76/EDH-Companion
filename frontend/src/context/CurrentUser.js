7579add40534839eaaba72fc71f40a81833dcde5
alex <alex.schmidt767@gmail.com>
2025-11-22 04:45:37 -0600

    created current user provider context and added its wrapper to the app

---
+++ frontend/src/context/CurrentUser.js
@@ -0,0 +1,26 @@
+// this context will provide the user that is currently 
+// logged-in to its children
+
+import { createContext, useState, useEffect } from "react";
+
+export const CurrentUser = createContext();
+
+const CurrentUserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/auth/current-user`, {
+            credentials: 'include'
+        })
+            .then(res => res.json())
+            .then(resData => setCurrentUser(resData))
+    }, []);
+
+    return (
+        <CurrentUser.Provider value={{ currentUser, setCurrentUser}}>
+            {children}
+        </CurrentUser.Provider>
+    );
+}
+
+export default CurrentUserProvider;
