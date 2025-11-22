b11f798a44acce1621cbc113e4161b0def49db72
alex <alex.schmidt767@gmail.com>
2025-11-22 14:45:29 -0600

    swithced to sub branch to get the frontend deployment working properly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vue from '@vitejs/plugin-vue-jsx'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
