050a8f2edcd94d8a50b2e6dd92c27a7c584bbe43
alex <alex.schmidt767@gmail.com>
2025-11-28 00:43:53 -0600

    added log out and get current user routes to auth

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -8,8 +8,8 @@ const User = require('../models/user');
 
 // authenticate user log in
 auth.post('/', async (req, res) => {
-    // find user with matching email/username
-    let user
+    // find user with matching email/username in db
+    let user;
     try {
         user = await User.findOne({
             where: {
@@ -19,7 +19,7 @@ auth.post('/', async (req, res) => {
                 ]
             }
         });
-     } catch (error) {
+    } catch (error) {
         res.status(500).json({
             error: {
                 error,
@@ -27,13 +27,14 @@ auth.post('/', async (req, res) => {
                 message: "Database error, try again in a few moments."
             }
         });
-     }
+    }
 
+    // confirm a user was found
     if (!user) {
         res.status(404).json({
             error: {
-                invalidUsername: true,
-                message: 'An account with this username/email does not exist.'
+                invalidCredentials: true,
+                message: 'There is no account with this combination of username/email and password.'
             }
         });
     } 
@@ -42,11 +43,51 @@ auth.post('/', async (req, res) => {
         res.status(404).json({
             error: {
                 invalidCredentials: true,
-                message: 'Incorrect Password'
+                message: 'There is no account with this combination of username/email and password.'
             }
         })
     }
-    
-    // TO DO: sign in user to session here
+
+    // log user in
+    req.session.userId = user.userId;
     res.status(200).json({ user });
 });
+
+// log user out
+auth.post('/log-out', async (req, res) => {
+    if (req.body.userId && req.body.userId === req.session.userId) {
+        req.session.userId = null;
+        res.status(200).json({
+            message: 'User signed out.'
+        });
+    }
+
+    res.status(401).json({
+        message: 'No user signed out.'
+    });
+});
+
+// get the current signed-in user
+auth.get('/current-user', async (req, res) => {
+    if (req.session.userId) {
+        try {
+            const user = await User.findOne({
+                where: { 
+                    id: req.session.userId
+                }
+            });
+    
+            if (user) res.status(200).json(user);
+        } catch (error) {
+            res.status(500).json({
+                error: {
+                    error,
+                    databaseError: true,
+                    message: "Database error, try again in a few moments."
+                }
+            });
+        }
+    }
+
+    res.status(204).json(null);
+})
