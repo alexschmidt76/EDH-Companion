bd0f09992a766a4fa7fe9b97b9ce43ec61ce04d0
alex <alex.schmidt767@gmail.com>
2025-11-28 15:29:42 -0600

    updating home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,18 @@ import Navigation from './components/Navigation';
 import Home from './components/Home';
 import SignUpForm from './components/users/SignUpForm';
 import LogInForm from './components/users/LogInForm';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route , useNavigate} from 'react-router-dom';
 
 function App() {
+  useNavigate()('/');
   return (
     <div className="App">
       <CurrentUserProvider>
         <Router>
           <Navigation/>
           <Routes>
-            <Route exact path="/" element={<Home/>}/>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path="/home" element={<Home/>}/>
             <Route exact path='/sign-up-form' element={<SignUpForm/>}/>
             <Route exact path='/log-in-form' element={<LogInForm/>}/>
           </Routes>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
+import { CurrentUser } from "../context/CurrentUser";
+
 const Home = () => {
-    return (
-        <div></div>
-    );
+    // redirect user to their own page if there is a user logged in
+    const navigate = useNavigate();
+    if (CurrentUser) navigate(`/user-page/:${CurrentUser.username}`);
+
+    // home will be an info page for the web app
+    return (<div>Give info on web app here in the future</div>);
 }
 
 export default Home;
