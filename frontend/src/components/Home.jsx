a0b1b6a103ba368897cc7cdcea22a285aa638a14
alex <alex.schmidt767@gmail.com>
2025-11-28 15:34:37 -0600

    added useContext to home page for rerouting

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { CurrentUser } from "../context/CurrentUser";
+import { useContext } from "react";
 
 const Home = () => {
     // redirect user to their own page if there is a user logged in
     const navigate = useNavigate();
-    if (CurrentUser) navigate(`/user-page/:${CurrentUser.username}`);
+    if (CurrentUser) navigate(`/user-page/:${useContext(CurrentUser).currentUser.username}`);
 
     // home will be an info page for the web app
     return (<div>Give info on web app here in the future</div>);
