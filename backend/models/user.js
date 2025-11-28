b683b00d66c5d3344807b21e5f479a73179bfd01
alex <alex.schmidt767@gmail.com>
2025-11-28 01:30:35 -0600

    editied users model to include association with future notification table and specified row names for its other associations

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,20 +3,43 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      User.belongsToMany(models.Game, { through: models.UserGame });
-      User.belongsToMany(models.Pod, { through: models.PodUser });
+      User.belongsToMany(models.Game, { 
+        as: 'games',
+        through: models.UserGame 
+      });
+
+      User.belongsToMany(models.Pod, { 
+        as: 'pods',
+        through: models.PodUser 
+      });
+
       User.belongsToMany(User, {
         as: "followers",
         through: models.UserFollower,
         foreignKey: 'FollowingId',
         otherKey: 'FollowerID'
-      })
+      });
+
       User.belongsToMany(User, {
         as: "following",
         through: models.UserFollower,
         foreignKey: "FollowerId",
         otherKey: "FollowingId"
-      })
+      });
+
+      User.belongsToMany(models.Notification, {
+        as: 'recievedNotifications',
+        through: models.Notification,
+        foreignKey: 'RecieverId',
+        otherKey: 'SenderId'
+      });
+
+      User.belongsToMany(models.Notification, {
+        as: 'sentNotifications',
+        through: models.Notification,
+        foreignKey: 'SenderId',
+        otherKey: 'RecieverId'
+      });
     }
   }
   User.init({
