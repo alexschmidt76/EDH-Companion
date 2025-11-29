e58b48db4b641cd1a875d2b7b60ec2e883dbb5e4
alex <alex.schmidt767@gmail.com>
2025-11-28 18:23:13 -0600

    added some info to currentuserpageheader

--- frontend/src/components/users/user_pages/CurrentUserPageHeader.jsx
+++ frontend/src/components/users/user_pages/CurrentUserPageHeader.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react"
+import { CurrentUser } from "../../../context/CurrentUser"
+import { ButtonToolbar } from "react-bootstrap";
+
 const CurrentUserPageHeader = () => {
+    const { currentUser } = useContext(CurrentUser);
+
     return (
         <div id="current-user-page-header">
-            
+            <h1>Hi, {currentUser.username}</h1>
+            <ButtonToolbar>
+                <Button href={`/user-page/${currentUser.username}/edit-account`}>Edit Account</Button>
+                <Button href={`/games/log-new-game`}>Log Game</Button>
+            </ButtonToolbar>
         </div>
     )
 }
