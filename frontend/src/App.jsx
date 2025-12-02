57893962e265d337feccbf901b80c862d0fd8dd0
alex <alex.schmidt767@gmail.com>
2025-12-01 22:56:05 -0600

    added UserSearchForm file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import SignUpForm from './components/users/forms/SignUpForm';
 import LogInForm from './components/users/forms/LogInForm';
 import UserPage from './components/users/user_pages/UserPage';
 import UserNotFoundPage from './components/users/user_pages/UserNotFoundPage';
+import UserSearchForm from './components/users/forms/UserSearchForm';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -22,12 +23,14 @@ function App() {
           <NavigationBar/>
           <Routes>
             {/* the paths '/' and '/home' will have the same destination */}
-            <Route exact path='/' element={<Home/>}/>
-            <Route exact path='/home' element={<Home/>}/>
-            <Route exact path='/sign-up-form' element={<SignUpForm/>}/>
-            <Route exact path='/log-in-form' element={<LogInForm/>}/>
-            <Route exact path='/user-page/:username/:activePage' element={<UserPage/>}/>
-            <Route exact path='/user-page/user-not-found' element={<UserNotFoundPage/>}/>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/sign-up-form' element={<SignUpForm/>}/>
+            <Route path='/log-in-form' element={<LogInForm/>}/>
+            <Route path='/user-page' element={<UserSearchForm/>}>
+              <Route path=':username/:activePage' element={<UserPage/>}/>
+              <Route path='user-not-found' element={<UserNotFoundPage/>}/>
+            </Route>
           </Routes>
         </Router>
       </CurrentUserProvider>
---
+++ frontend/src/components/users/forms/UserSearchForm.jsx
@@ -0,0 +1,5 @@
+const UserSearchForm = () => {
+    return <></>
+}
+
+export default UserSearchForm;
