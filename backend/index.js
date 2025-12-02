701e1576d44432bafccdf95aa177c3253c1cd793
alex <alex.schmidt767@gmail.com>
2025-12-01 23:17:46 -0600

    testing headers

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requeste-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requeste-With, Content-Type, Accept");
     next();
 });
 app.use(express.static('public'));
